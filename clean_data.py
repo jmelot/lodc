607ef7af53565878991826968b87bb2f62b58559
Jennifer Melot <jtmelot@gmail.com>
2022-12-31 18:54:20 -0500

    Normalize the normalization functions

--- clean_data.py
+++ clean_data.py
@@ -1,31 +1,69 @@
 import csv
 import re
 
-def clean_address(addr):
-    clean = addr.replace(", NW", " NW").replace(", NE", " NE").replace(", SE", " SE").replace(", SW", " SW").replace(".", "")
-    clean = re.sub(r"(NE|NW|SE|SW).*", r"\1", clean).strip()
+ADDRESS_REPLACEMENTS = [("Consitution", "Constitution"),
+                        ("Maine SW", "Maine Ave SW"),
+                        ("First", "1st"),
+                        ("Second", "2nd"),
+                        ("Third", "3rd"),
+                        ("Fourth", "4th"),
+                        ("Fifth", "5th"),
+                        ("Sixth", "6th"),
+                        ("Seventh", "7th"),
+                        ("Eighth", "8th"),
+                        ("Ninth", "9th"),
+                        ("Street", "St"),
+                        ("Avenue", "Ave")
+                       ]
+BIRD_REPLACEMENTS = [("?", ""), ("sp.", "species"), ("Rose breasted ", "Rose-breasted ")]
+DIRECTIONS = ["NE", "NW", "SE", "SW"]
+
+
+def clean_address(addr: str) -> str:
+    """
+    Normalize address string
+    :param addr: address string to be normalized
+    :return: normalized address
+    """
+    clean = addr.replace(".", "")
+    for direct in DIRECTIONS:
+        clean = clean.replace(f", {direct}", f" {direct}")
+    clean = re.sub(rf"({'|'.join(DIRECTIONS)}).*", r"\1", clean).strip()
     parts = clean.split(",")
-    street_parts = [p for p in parts if "NE" in p or "NW" in p or "SE" in p or "SW" in p]
+    street_parts = [p for p in parts if any([direct in p for direct in DIRECTIONS])]
     if len(street_parts) > 0:
         clean = street_parts[0]
     elif len(parts) > 0:
         clean = parts[0]
     paren_parts = [p.replace(")", "") for p in clean.split("(")]
-    street_paren_parts = [p for p in paren_parts if "NE" in p or "NW" in p or "SE" in p or "SW" in p]
+    street_paren_parts = [p for p in paren_parts if any([direct in p for direct in DIRECTIONS])]
     if len(street_paren_parts) > 0:
         clean = street_paren_parts[0]
     elif len(paren_parts) > 0:
         clean = paren_parts[0]
-    clean = clean.replace("Consitution", "Constitution").replace("Maine SW", "Maine Ave SW").replace("First", "1st").replace("Street", "St").replace("Avenue", "Ave")
+    for s_from, s_to in ADDRESS_REPLACEMENTS:
+        clean = clean.replace(s_from, s_to)
     return clean.strip()
 
-def clean_bird(bird):
-    bird = bird.split("(")[0].split(",")[0].replace("?",
-            "").replace("sp.", "species").replace("Rose breasted ", "Rose-breasted ").strip()
-    return bird
+
+def clean_bird(bird: str) -> str:
+    """
+    Normalize name of bird
+    :param bird: Original name of the bird
+    :return: Normalized name of the bird
+    """
+    bird = bird.split("(")[0].split(",")[0]
+    for from_s, to_s in BIRD_REPLACEMENTS:
+        bird = bird.replace(from_s, to_s)
+    return bird.strip()
+
 
 if __name__ == "__main__":
-    out = csv.DictWriter(open("clean.csv", mode="w"), fieldnames=["Date", "Bird Species, if known", "Clean Bird Species", "Sex, if known", "Address where found", "Clean Address", "CW Number", "Disposition", "Status: Released-- Nearest address/landmark", "Last Name", "What route?", "Status", "Approx. time you found the bird"])
+    out = csv.DictWriter(open("clean.csv", mode="w"),
+                         fieldnames=["Date", "Bird Species, if known", "Clean Bird Species", "Sex, if known",
+                                     "Address where found", "Clean Address", "CW Number", "Disposition",
+                                     "Status: Released-- Nearest address/landmark", "Last Name", "What route?",
+                                     "Status", "Approx. time you found the bird"])
     out.writeheader()
 
     address_to_bird = {}
@@ -42,7 +80,7 @@ if __name__ == "__main__":
             address_to_bird[cleaned_addr][cleaned_bird] = address_to_bird[cleaned_addr].get(cleaned_bird, 0)+1
 
     with open("bldg_counts.csv", mode="w") as f:
-        writer = csv.DictWriter(f, fieldnames = ["Building", "Bird", "Count"])
+        writer = csv.DictWriter(f, fieldnames=["Building", "Bird", "Count"])
         writer.writeheader()
         for address in sorted(address_to_bird.keys()):
             for bird in sorted(address_to_bird[address].keys()):
